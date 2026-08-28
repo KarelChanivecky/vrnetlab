360b70c6a7ed451597acfd465783874547abee4d
kchaniveckyga <kchaniveckyga@fortinet.com>
2026-08-28 11:17:30 -0700

    Disable onboarding by default

--- fortinet/fortigate/docker/features/default_config.py
+++ fortinet/fortigate/docker/features/default_config.py
@@ -1,18 +1,29 @@
 """Default FortiOS settings feature."""
 
+import os
+
 from cli_commands import ConfigBlock
 
 from .base import StaticFeature
 
 
+def onboarding_enabled():
+    return os.getenv("FOS_ONBOARDING", "false").lower() == "true"
+
+
 class DefaultConfig(StaticFeature):
     def __init__(self, vm, commander):
         self._hostname_line = f"set hostname {vm.hostname}"
+        global_config = ["set admin-scp enable"]
+        if onboarding_enabled():
+            global_config.extend([
+                "set admin-https-redirect disable",
+                "set gui-auto-upgrade-setup-warning disable",
+            ])
+        global_config.append(self._hostname_line)
+
         super().__init__(vm, commander, "default-config", [
-            ConfigBlock("system global", [
-                "set admin-scp enable",
-                self._hostname_line,
-            ]),
+            ConfigBlock("system global", global_config),
             ConfigBlock("system fortiguard", [
                 "set auto-join-forticloud disable",
             ])
